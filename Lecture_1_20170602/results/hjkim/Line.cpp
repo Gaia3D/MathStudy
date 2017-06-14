c9517504e9f90a986f1e05e934a0b4d5e952dd48
shire75 <hjkim@gaia3d.com>
2017-06-14 10:18:36 +0900

    Create Line.cpp

--- Lecture_1_20170602/results/hjkim/Line.cpp
+++ Lecture_1_20170602/results/hjkim/Line.cpp
@@ -34,4 +34,23 @@ double Line::distanceTo(Point3D& target)
 {
 	Point3D v1 = startPoint - target;
 	Point3D v2 = endPoint - target;
+
+	Point3D v2to1 = v1 - v2;
+
+	double numerator = v2to1.x * v2.x + v2to1.y * v2.y + v2to1.z + v2.z;
+	double denominator = v2to1.magnitude() * v2to1.magnitude();
+	double param = -numerator / denominator;
+
+	if (param <= 0.0)
+	{
+		return v2.magnitude();
+	}
+	else if (param >= 1.0)
+	{
+		return v1.magnitude();
+	}
+	else
+	{
+		return sqrt(param*numerator + v2.magnitude()*v2.magnitude());
+	}
 }
